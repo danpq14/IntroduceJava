43e1a886962bbaad8c491e4ce9606f5aeffafe75
danpq14 <61695301+danpq14@users.noreply.github.com>
2020-04-08 20:59:36 +0700

    Khử mã lặp bài tính số ngày trong tháng

--- src/PhuongTrinhBac1.java
+++ src/PhuongTrinhBac1.java
@@ -1,2 +1,26 @@
+import java.util.Scanner;
+
 public class PhuongTrinhBac1 {
+    public static void main(String[] args) {
+        System.out.println("Chương trình tính nghiệm phương trình bậc 1");
+        System.out.println("Mời nhập vào các Hằng số Ax + B = 0");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("A : ");
+        double a = scanner.nextDouble();
+        System.out.println("B : ");
+        double b = scanner.nextDouble();
+
+        if (a == 0) {
+            if (b == 0) {
+                System.out.println("Phương trình có vô số nghiệm");
+            } else {
+                System.out.println("Phương trình vô Nghiệm");
+            }
+        }
+        else {
+            double x = -b/a;
+            System.out.println("Phương trình có nghiệm là x = "+ x);
+        }
+    }
 }
--- src/TinhSoNgayTrongThang.java
+++ src/TinhSoNgayTrongThang.java
@@ -1,4 +1,41 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class TinhSoNgayTrongThang {
+    public static void main(String[] args) {
+        System.out.println("Chương trình tính số ngày trong 1 tháng");
+        int month;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập vào tháng mà bạn muốn kiểm tra : ");
+        month = scanner.nextInt();
+        String dayOfMonth;
+
+        switch (month) {
+            case 1 :
+            case 3 :
+            case 5 :
+            case 7 :
+            case 8 :
+            case 10 :
+            case 12 :
+                dayOfMonth = "31";
+                break;
+            case 4 :
+            case 6 :
+            case 9 :
+            case 11 :
+                dayOfMonth = "30";
+                break;
+            case 2 :
+                dayOfMonth = "28 hoặc 29";
+                break;
+
+            default:
+                dayOfMonth = "Bạn nhập ko chính xác";
+        }
+        if (dayOfMonth != "Bạn nhập ko chính xác") {
+            System.out.printf("Tháng 3%d có %s ngày", month, dayOfMonth);
+        } else  {
+            System.out.println(dayOfMonth);
+        }
+    }
 }

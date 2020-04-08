f40afafffa0b44c1e1999a9bc5172694f71172e7
danpq14 <61695301+danpq14@users.noreply.github.com>
2020-04-08 20:13:12 +0700

    bai tap khai bao bien

---
+++ src/LuyenTapKhaiBaoBien.java
@@ -0,0 +1,14 @@
+public class LuyenTapKhaiBaoBien {
+    public static void main(String[] args) {
+        int i = 10;
+        double f =20.5;
+        boolean d = false;
+        char c = 'd';
+        String dan = "Dân đẹp trai";
+        System.out.println(i);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(c);
+        System.out.println(dan);
+    }
+}
